41f06f1122cfdbf407a87775473eb14ab9feb9fe
Christoph Bühler <christoph.buehler@unisg.ch>
2025-03-27 13:09:10 +0100

    debounce NFC events

--- src/sensors/internal/events/nfc.go
+++ src/sensors/internal/events/nfc.go
@@ -2,6 +2,8 @@ package events
 
 import (
 	"encoding/json"
+	"sync"
+	"time"
 
 	sensorsv1 "github.com/buehler/mcs-event-driven-systems/sensors/gen/events/sensors/v1"
 	"github.com/buehler/mcs-event-driven-systems/sensors/internal/publisher"
@@ -15,6 +17,29 @@ type NFC struct {
 }
 
 var lastReading *string
+
+func debounce(f func(), delay time.Duration) func() {
+	var mu sync.Mutex
+	var timer *time.Timer
+
+	return func() {
+		mu.Lock()
+		defer mu.Unlock()
+
+		// Stop existing timer if it exists
+		if timer != nil {
+			timer.Stop()
+		}
+		// Start a new timer that calls f after delay
+		timer = time.AfterFunc(delay, f)
+	}
+}
+
+var debouncedRemovedEvent = debounce(func() {
+	publisher.SendKafkaEvent(&sensorsv1.NFCObjectRemoved{})
+	lastReading = nil
+	logrus.Info("NFC object removed")
+}, 1*time.Second+100*time.Millisecond)
 
 func OnNFCMessageReceived(_ mqtt.Client, msg mqtt.Message) {
 	logger := logrus.WithField("topic", msg.Topic())
@@ -31,9 +56,7 @@ func OnNFCMessageReceived(_ mqtt.Client, msg mqtt.Message) {
 		lastReading = event.ID
 		logger.Info("NFC object detected")
 	} else if lastReading != nil && event.ID == nil {
-		publisher.SendKafkaEvent(&sensorsv1.NFCObjectRemoved{})
-		lastReading = nil
-		logger.Info("NFC object removed")
+		debouncedRemovedEvent()
 	} else {
 		logger.Warn("NFC object state not changed")
 	}
