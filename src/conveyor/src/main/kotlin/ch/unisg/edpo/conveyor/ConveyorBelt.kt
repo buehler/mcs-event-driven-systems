c3565528437db70946cb4fe77b68c1d5cb110ba9
Christoph Bühler <christoph.buehler@unisg.ch>
2025-03-27 11:22:43 +0100

    conveyor move stuff

--- src/conveyor/src/main/kotlin/ch/unisg/edpo/conveyor/ConveyorBelt.kt
+++ src/conveyor/src/main/kotlin/ch/unisg/edpo/conveyor/ConveyorBelt.kt
@@ -1,13 +1,12 @@
 package ch.unisg.edpo.conveyor
 
 import khttp.get
-import kotlinx.coroutines.delay
 import org.slf4j.LoggerFactory
 import org.springframework.beans.factory.annotation.Value
 import org.springframework.stereotype.Service
 
 enum class ConveyorDirection(val value: Int) {
-    LEFT(1), RIGHT(-1)
+    LEFT(1), RIGHT(0)
 }
 
 @Service
@@ -17,37 +16,37 @@ class ConveyorBelt {
     @Value("\${conveyor.server}")
     private val server: String = ""
 
-    @Value("\${conveyor.time-at-base-speed}")
-    private val timeAtBaseSpeed = 0L
+    @Value("\${conveyor.min-speed}")
+    private val minSpeed = 0
+
+    @Value("\${conveyor.max-speed}")
+    private val maxSpeed = 0
+
+    @Value("\${conveyor.distance}")
+    private val distance = 0
+
+    var speed = 0
+        set(value) {
+            val calcSpeed = minSpeed + (((value * -1) + maxSpeed) / 6)
+            field = when {
+                calcSpeed < minSpeed -> minSpeed
+                calcSpeed > maxSpeed -> maxSpeed
+                else -> calcSpeed
+            }
+        }
 
     suspend fun moveBlock() {
         logger.info("Moving block on conveyor from picker to color sensor robot")
 
-        startConveyor()
-        delay(timeAtBaseSpeed)
-        stopConveyor()
-    }
-
-    private fun startConveyor() {
         val direction = ConveyorDirection.LEFT
         logger.info("Starting conveyor on server $server in direction ${direction.name}")
 
-        val response = get("http://$server/conveyor?speed=1&direction=${direction.value}")
+        val response =
+            get("http://$server/move_conveyor?speed-mm-s=$speed&distance-mm=$distance&direction=${direction.value}")
         if (response.statusCode > 299) {
             logger.error("Failed to start conveyor on server $server: ${response.statusCode} - ${response.text}")
         } else {
             logger.info("Conveyor started successfully on server $server")
-        }
-    }
-
-    private fun stopConveyor() {
-        logger.info("Stopping conveyor on server $server")
-
-        val response = get("http://$server/conveyor?speed=0&direction=1")
-        if (response.statusCode > 299) {
-            logger.error("Failed to stop conveyor on server $server: ${response.statusCode} - ${response.text}")
-        } else {
-            logger.info("Conveyor stopped successfully on server $server")
         }
     }
 }
--- src/conveyor/src/main/kotlin/ch/unisg/edpo/conveyor/listeners/CommandListener.kt
+++ src/conveyor/src/main/kotlin/ch/unisg/edpo/conveyor/listeners/CommandListener.kt
@@ -2,6 +2,7 @@ package ch.unisg.edpo.conveyor.listeners
 
 import ch.unisg.edpo.conveyor.ConveyorBelt
 import ch.unisg.edpo.conveyor.producer.EventProducer
+import ch.unisg.edpo.proto.events.sensors.v1.ConveyorSpeedChanged
 import org.slf4j.LoggerFactory
 import org.springframework.kafka.annotation.KafkaListener
 import org.springframework.messaging.handler.annotation.Header
@@ -13,7 +14,7 @@ class CommandListener(private val conveyorBelt: ConveyorBelt, private val eventP
     private val logger = LoggerFactory.getLogger(javaClass)
 
     @KafkaListener(
-        topics = ["\${topics.commands}"],
+        topics = ["\${topics.commands}", "\${topics.events}"],
         containerFactory = "kafkaListenerFactory"
     )
     suspend fun listen(@Header("messageType") messageType: String, @Payload payload: ByteArray) {
@@ -22,6 +23,11 @@ class CommandListener(private val conveyorBelt: ConveyorBelt, private val eventP
                 logger.info("Received command to move block")
                 conveyorBelt.moveBlock()
                 eventProducer.sendBlockMoved()
+            }
+            "ConveyorSpeedChanged"-> {
+                val cmd = ConveyorSpeedChanged.parseFrom(payload)
+                logger.info("Received command to change speed to ${cmd.speed}")
+                conveyorBelt.speed = cmd.speed
             }
 
             else -> logger.debug("Received command with type $messageType")
