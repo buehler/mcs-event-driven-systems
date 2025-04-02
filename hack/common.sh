8c864637c7fbea0f0cf77aca50d0c0e70307b273
Christoph Bühler <christoph.buehler@unisg.ch>
2025-04-02 09:28:33 +0200

    start helper scripts

---
+++ hack/common.sh
@@ -0,0 +1,10 @@
+#/bin/bash
+
+function sendEvent() {
+    topic=$1
+    event=$2
+    payload=$3
+
+    echo "Send event '$event' to topic '$topic' with payload '$payload'."
+    docker compose run --rm test-publisher $topic $event $payload
+}
