a8243f403cb8f30d676c050c31329abd9921924f
Christoph Bühler <christoph.buehler@unisg.ch>
2025-05-01 18:02:24 -0400

    add windowed store

---
+++ src/streams/src/main/kotlin/ch/unisg/edpo/streams/WindowedBlocksHandledTable.kt
@@ -0,0 +1,41 @@
+package ch.unisg.edpo.streams
+
+import ch.unisg.edpo.proto.events.machines.v1.BlockSorted
+import ch.unisg.edpo.proto.models.v1.BlockColor
+import org.apache.kafka.common.serialization.Serdes
+import org.apache.kafka.common.utils.Bytes
+import org.apache.kafka.streams.StreamsBuilder
+import org.apache.kafka.streams.kstream.*
+import org.apache.kafka.streams.state.WindowStore
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Component
+import java.time.Duration
+
+@Component
+class WindowedBlocksHandledTable {
+    @Value("\${topics.events}")
+    private val eventsTopic = ""
+
+    @Autowired
+    fun windowedBlocksHandledTable(sb: StreamsBuilder): KTable<Windowed<BlockColor>, Int> {
+        val table = sb
+            .stream(eventsTopic, Consumed.with(Serdes.String(), GeneratedMessageSerdes()))
+            .filter { _, v -> v is BlockSorted }
+            .groupBy(
+                { _, v -> (v as BlockSorted).color },
+                Grouped.with(EnumSerde(BlockColor::class.java), GeneratedMessageSerdes())
+            )
+            .windowedBy(TimeWindows.ofSizeWithNoGrace(Duration.ofMinutes(15)))
+            .aggregate(
+                { -> 0 },
+                { _, _, agg -> agg + 1 },
+                Materialized
+                    .`as`<BlockColor, Int, WindowStore<Bytes, ByteArray>>("windowed-blocks-handled-table")
+                    .withKeySerde(EnumSerde(BlockColor::class.java))
+                    .withValueSerde(Serdes.Integer()),
+            )
+
+        return table
+    }
+}
